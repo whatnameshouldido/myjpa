0c9df9426ef13c8b1775297d5b9dcc5882bae0e7
whatnameshouldido <pobyjun@naver.com>
2024-07-11 14:13:51 +0900

    plus test
    plus test

--- src/test/java/com/studymavernspringboot/myjpa/Category/CategoryControllerTest.java
+++ src/test/java/com/studymavernspringboot/myjpa/Category/CategoryControllerTest.java
@@ -1,6 +1,7 @@
 package com.studymavernspringboot.myjpa.Category;
 
 import com.studymavernspringboot.myjpa.category.CategoryDto;
+import com.studymavernspringboot.myjpa.category.ICategory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,17 +24,62 @@ public class CategoryControllerTest {
     public void CategoryTest() {
         String url = "http://localhost:" + port;
         CategoryDto requestInsert = CategoryDto.builder().build();
-        ResponseEntity<CategoryDto> responseInsert = this.testRestTemplate.postForEntity(url + "/cat"
+        ResponseEntity<CategoryDto> responseInsert = this.testRestTemplate.postForEntity(
+                url + "/cat"
                 ,requestInsert, CategoryDto.class);
         assertThat(responseInsert).isNotNull();
         assertThat(responseInsert.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
 
-        CategoryDto requestInsert2 = CategoryDto.builder().name("hi bro").build();
-        ResponseEntity<CategoryDto> responseInsert2 = this.testRestTemplate.postForEntity(url + "/cat"
+        CategoryDto requestInsert2 = CategoryDto.builder().name("RestFull").build();
+        ResponseEntity<CategoryDto> responseInsert2 = this.testRestTemplate.postForEntity(
+                url + "/cat"
                 ,requestInsert2, CategoryDto.class);
         assertThat(responseInsert2).isNotNull();
         assertThat(responseInsert2.getStatusCode()).isEqualTo(HttpStatus.OK);
         System.out.println("responseInsert2.getBody().getId() = " + responseInsert2.getBody().getId());
-        assertThat(responseInsert2.getBody().getName()).isEqualTo("hi bro");
+        assertThat(responseInsert2.getBody().getName()).isEqualTo("RestFull");
+
+        Long insertId = responseInsert2.getBody().getId();
+        ResponseEntity<CategoryDto> findEntity = this.testRestTemplate.getForEntity(
+                url + "/cat/" + insertId.toString()
+                , CategoryDto.class
+        );
+        assertThat(findEntity).isNotNull();
+        assertThat(findEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        ICategory resultFind = findEntity.getBody();
+        assertThat(resultFind).isNotNull();
+        assertThat(resultFind.getId()).isEqualTo(insertId);
+        assertThat(resultFind.getName()).isEqualTo("RestFull");
+
+        ResponseEntity<CategoryDto> notfindEntity = this.testRestTemplate.getForEntity(
+                url + "/cat/99999999"
+                , CategoryDto.class
+        );
+        assertThat(notfindEntity).isNotNull();
+        assertThat(notfindEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+
+        //Category update test
+        ICategory update = CategoryDto.builder().build();
+        update.copyFields(resultFind);
+        update.setName("NoRest");
+        CategoryDto resultObject = this.testRestTemplate.patchForObject(
+                url + "/cat/" + update.getId()
+                , update
+                , CategoryDto.class
+        );
+        assertThat(resultObject).isNotNull();
+        assertThat(resultObject.getName()).isEqualTo("NoRest");
+
+        //Category delete test
+        ICategory delete = CategoryDto.builder().id(update.getId()).build();
+        this.testRestTemplate.delete(url + "/cat/" + delete.getId());
+
+        ResponseEntity<CategoryDto> deleteEntity = this.testRestTemplate.getForEntity(
+                url + "/cat/" + delete.getId()
+                , CategoryDto.class
+        );
+        assertThat(deleteEntity).isNotNull();
+        assertThat(deleteEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
